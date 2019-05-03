fae2957761d6c26e97feb91e1b6b6780863a46b8
Zuguang Chen <airborn@qq.com>
2019-05-03 16:35:03 +1000

    Set up the backgrond parameters

---
+++ Assets/Scripts/ItemControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemControl : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D myRigidbody;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
